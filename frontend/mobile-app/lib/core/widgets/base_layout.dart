4d9b0369ba91462dbb388ec6473c28c36edc962f
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-03-30 11:52:09 -0500

    feat(mobile-app): add BaseLayout widget as base screen structure - Implements reusable scaffold with AppBarWidget and horizontal padding

---
+++ frontend/mobile-app/lib/core/widgets/base_layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_app/core/theme/app_spacing.dart';
+import 'package:mobile_app/core/widgets/app_bar_widget.dart';
+
+class BaseLayout extends StatelessWidget{
+  
+  final String title;
+  final Widget child;
+
+  const BaseLayout({
+    super.key,
+    required this.title,
+    required this.child
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(title: title),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: AppSpacing.xl),
+      ),
+    );
+  }
+
+}
