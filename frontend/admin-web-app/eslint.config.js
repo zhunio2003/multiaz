209d58b88518a6b33869d256372fdc5f59807fa7
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-03-31 06:12:11 -0500

    feat: configure ESLint for React and typescript

--- frontend/admin-web-app/eslint.config.js
+++ frontend/admin-web-app/eslint.config.js
@@ -4,20 +4,28 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/App.tsx']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      }
     },
   },
 ])
