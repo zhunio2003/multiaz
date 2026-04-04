b1c2422e64f5419859ee48e219e2120c75eca770
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-04-03 22:47:52 -0500

    feat(admin-web-app): add auth Interceptor with JWT injection

--- frontend/admin-web-app/src/core/network/apiClient.ts
+++ frontend/admin-web-app/src/core/network/apiClient.ts
@@ -9,4 +9,12 @@ const apiClient = axios.create({
 
 })
 
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('accessToken')
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config 
+});
+
 export default apiClient;
