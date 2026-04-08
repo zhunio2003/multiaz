325588d31bb63e2e2a8cec2a3dc655d9b63711c1
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-04-07 23:59:48 -0500

    feat(api-gateway): add CorsConfig to allow cross-origin requests

---
+++ backend/core/api-gateway/src/main/java/com/multiaz/apigateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.multiaz.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
