0df214f36a8fc498da931286cf713913e250b3e9
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-04-07 23:32:04 -0500

    feat(api-gateway): add JwtAuthFilter to api-gateway - add jjwt dependencies - add JWT secret key to application.yml - configuration JwtAuthFilter class

---
+++ backend/core/api-gateway/src/main/java/com/multiaz/apigateway/filter/JwtAuthFilter.java
@@ -0,0 +1,74 @@
+package com.multiaz.apigateway.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+@Order(1)
+public class JwtAuthFilter implements Filter{
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    private static final List<String> PUBLIC_ROUTES = List.of(
+        "/auth/register",
+        "/auth/login",
+        "/auth/recover"
+    );
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        
+        String path = req.getRequestURI();
+        
+        if(PUBLIC_ROUTES.contains(path)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String authHeader = req.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+
+
+    }
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+}
