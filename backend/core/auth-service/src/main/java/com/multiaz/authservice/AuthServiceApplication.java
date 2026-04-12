458f0dea76a24810bc27cf0b0482962142495ab5
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-04-11 20:20:55 -0500

    fix(auth-service): exclude UserDetailsServiceAutoConfiguration

--- backend/core/auth-service/src/main/java/com/multiaz/authservice/AuthServiceApplication.java
+++ backend/core/auth-service/src/main/java/com/multiaz/authservice/AuthServiceApplication.java
@@ -2,8 +2,11 @@ package com.multiaz.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = UserDetailsServiceAutoConfiguration.class
+)
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
