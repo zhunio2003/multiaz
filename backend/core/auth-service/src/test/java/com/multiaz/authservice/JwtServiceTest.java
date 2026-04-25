fa39d14eaf92055ae4002f1f778d3ddb615083fd
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-04-24 20:59:25 -0500

    test(auth-service): implement unit test for generated AccessToken with correct claims

---
+++ backend/core/auth-service/src/test/java/com/multiaz/authservice/JwtServiceTest.java
@@ -0,0 +1,68 @@
+package com.multiaz.authservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.crypto.SecretKey;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.multiaz.authservice.model.Role;
+import com.multiaz.authservice.model.User;
+import com.multiaz.authservice.security.JwtService;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@ExtendWith(MockitoExtension.class)
+public class JwtServiceTest {
+
+  private JwtService jwtService;
+
+  @BeforeEach
+  void setup() {
+    jwtService = new JwtService();
+
+    ReflectionTestUtils.setField(jwtService, "secretString", "dGVzdFNlY3JldEtleUZvckp3dFRlc3RpbmdQdXJwb3Nlc09ubHkxMjM0NTY3OA==");
+
+  }
+
+  @Test
+  void generatedAccessTokenWithCorrectClaims() {
+
+    SecretKey key  = Keys.hmacShaKeyFor(Decoders.BASE64.decode("dGVzdFNlY3JldEtleUZvckp3dFRlc3RpbmdQdXJwb3Nlc09ubHkxMjM0NTY3OA"));
+
+    User user = User.builder()
+                  .id(UUID.randomUUID())
+                  .name("test")
+                  .email("test@gmail.com")
+                  .roles(Set.of(Role.builder().name("CUSTOMER").build()))
+                .build();
+
+    String token = jwtService.generateAccessToken(user);
+
+    Claims claims = Jwts.parser()
+                      .verifyWith(key)
+                      .build()
+                      .parseSignedClaims(token)
+                      .getPayload();
+    
+    assertEquals(user.getId().toString(), claims.getSubject());
+    assertEquals(user.getEmail(), claims.get("email", String.class));
+
+  }
+
+  
+
+}
