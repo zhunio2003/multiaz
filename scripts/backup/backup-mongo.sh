7c4bf642f0384f91b4d11b1ca8d658aabcc5cd4c
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-04-28 08:36:03 -0500

    feat(backup): add automated MongoDB backup scripts

---
+++ scripts/backup/backup-mongo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MG_HOST="mongodb"
+MG_USER=${MONGODB_USER}
+MG_PASSWORD=${MONGODB_PASS}
+
+DATABASES=("prediction-storage-details" "training" "logging-monitoring")
+
+FECHA=$(date +%Y%m%d_%H%M%S)
+
+for DB in "${DATABASES[@]}"; do
+  
+  mongodump --host $MG_HOST --username $MG_USER --password $MG_PASSWORD \
+    --authenticationDatabase admin \
+    --db $DB --out /backups/tmp/${DB}_${FECHA}
+
+  if [ $? -ne 0 ]; 
+    echo "ERROR backup de $DB FALLO"
+  else
+    tar -czf /backups/${DB}_${FECHA}.tar.gz -C /backups/tmp ${DB}_${FECHA}
+    rm -rf /backups/tmp/${DB}_${FECHA}
+    echo "Backup de  $DB completado: /backups/${DB}_${FECHA}.tar.gz"
+  fi
+done
