5f3054c6458a39781edc28b1e892208a1d268ba9
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-04-30 13:30:12 -0500

    feat(model-registry-service): add model-registry-service - configuration eureka and config-service to application.yml

---
+++ backend/core/model-registry-service/src/main/java/com/multiaz/modelregistry_service/ModelRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.multiaz.modelregistry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/core/model-registry-service/src/test/java/com/multiaz/modelregistry_service/ModelRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiaz.modelregistry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
