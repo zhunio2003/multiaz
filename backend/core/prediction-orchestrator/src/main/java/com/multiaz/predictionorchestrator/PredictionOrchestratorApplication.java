02a6eb6c0f51be025dff2288ebb2110ea1a7f7fc
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-05-04 13:26:10 -0500

    feat(prediction-orchestrator): add service to backend/core - add Spring Web, Spring Reactive Web, Spring Data JPA, PostgreSQL Driver, Eureka Discovery Client, Config Client, Spring Boot Actuator and Lombok - add config-service and service-discovery configurations, name and ports to application.yml

---
+++ backend/core/prediction-orchestrator/src/main/java/com/multiaz/predictionorchestrator/PredictionOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.multiaz.predictionorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionOrchestratorApplication.class, args);
+	}
+
+}
---
+++ backend/core/prediction-orchestrator/src/test/java/com/multiaz/predictionorchestrator/PredictionOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiaz.predictionorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
