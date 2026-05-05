f25a4565ae5d9dec77de8a074a9e5a6ba98d59c3
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-05-05 06:40:10 -0500

    feat(prediction-orchestrator): add database prediction_orchestrator - add Flyway dependencies - add flyway configurations to appication.yml - add predictions table to V1__create_predictions_table.sql

---
+++ backend/core/prediction-orchestrator/src/main/resources/db/migration/V1__create_predictions_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE predictions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id VARCHAR(255) NOT NULL,
+    model_id VARCHAR(255) NOT NULL,
+    model_name VARCHAR(255)NOT NULL,
+    status VARCHAR(255)NOT NULL,
+    input_data JSONB NOT NULL,
+    output_data JSONB NOT NULL,
+    created_at TIMESTAMP NOT NULL, 
+    completed_at TIMESTAMP
+);
