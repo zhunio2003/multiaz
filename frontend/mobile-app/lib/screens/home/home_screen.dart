dd0917342b6a52b91166aafea73c808756a2fc18
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-05-23 23:40:43 -0500

    feat(frontend/mobile-app): add __buildCarousel_method to  HomeScreen

--- frontend/mobile-app/lib/screens/home/home_screen.dart
+++ frontend/mobile-app/lib/screens/home/home_screen.dart
@@ -4,16 +4,13 @@ import 'package:mobile_app/core/theme/app_spacing.dart';
 import 'package:mobile_app/core/theme/app_typography.dart';
 
 class HomeScreen extends StatefulWidget {
-
   const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   int _selectIndex = 0;
 
   @override
@@ -28,48 +25,53 @@ class _HomeScreenState extends State<HomeScreen> {
               _buildStats(),
               _buildBanner(),
               _buildCTA(),
+              _buildCarousel()
             ],
           ),
-        )
+        ),
       ),
     );
   }
 
-
   Widget _buildHeader() {
-    return Padding (
-      padding: EdgeInsets.symmetric(horizontal: AppSpacing.xs, vertical: AppSpacing.xs),
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: AppSpacing.xs,
+        vertical: AppSpacing.xs,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Text(
-                  "Buenos dias", 
-                  style: AppTypography.h6.copyWith(color: AppColors.onSurface)
-                ),
-                Text(
-                  "Miguel",
-                  style: AppTypography.h6.copyWith(color: AppColors.onBackground),
-                )
-              ]
-
-            ),
-            CircleAvatar(
-              backgroundColor: AppColors.surface,
-              child: Icon(
-                Icons.notifications_outlined,
-                color: AppColors.onSurface,
+        children: [
+          Column(
+            children: [
+              Text(
+                "Buenos dias",
+                style: AppTypography.h6.copyWith(color: AppColors.onSurface),
               ),
+              Text(
+                "Miguel",
+                style: AppTypography.h6.copyWith(color: AppColors.onBackground),
+              ),
+            ],
+          ),
+          CircleAvatar(
+            backgroundColor: AppColors.surface,
+            child: Icon(
+              Icons.notifications_outlined,
+              color: AppColors.onSurface,
             ),
-          ],
-      )
+          ),
+        ],
+      ),
     );
   }
 
   Widget _buildStats() {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: AppSpacing.xs, vertical: AppSpacing.xs),
+      padding: EdgeInsets.symmetric(
+        horizontal: AppSpacing.xs,
+        vertical: AppSpacing.xs,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -83,23 +85,28 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Column(
                 children: [
                   Text(
-                  "Predicciones", 
-                  style: AppTypography.body.copyWith(color: AppColors.onSurface)
-                ),
-                Text(
-                  "142",
-                  style: AppTypography.body.copyWith(color: AppColors.onBackground),
-                ),
-                Text(
-                  "+12 esta semana",
-                  style: AppTypography.body.copyWith(color: AppColors.primary),
-                ),
-                
+                    "Predicciones",
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.onSurface,
+                    ),
+                  ),
+                  Text(
+                    "142",
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.onBackground,
+                    ),
+                  ),
+                  Text(
+                    "+12 esta semana",
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.primary,
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
           ),
-          SizedBox(width: AppSpacing.sm,),
+          SizedBox(width: AppSpacing.sm),
           Expanded(
             child: Container(
               decoration: BoxDecoration(
@@ -110,29 +117,38 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Column(
                 children: [
                   Text(
-                  "Última predicción", 
-                  style: AppTypography.body.copyWith(color: AppColors.onSurface)
+                    "Última predicción",
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.onSurface,
+                    ),
                   ),
                   Text(
                     "Clasificador v2",
-                    style: AppTypography.body.copyWith(color: AppColors.onBackground),
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.onBackground,
+                    ),
                   ),
                   Text(
                     "Hace 2 horas",
-                    style: AppTypography.body.copyWith(color: AppColors.primary),
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.primary,
+                    ),
                   ),
                 ],
               ),
-            )
+            ),
           ),
         ],
       ),
-      );
+    );
   }
 
   Widget _buildBanner() {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: AppSpacing.lg, vertical: AppSpacing.lg),
+      padding: EdgeInsets.symmetric(
+        horizontal: AppSpacing.lg,
+        vertical: AppSpacing.lg,
+      ),
       child: Container(
         decoration: BoxDecoration(
           color: Color(0xFF0A2E1E),
@@ -142,44 +158,46 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: EdgeInsets.all(AppSpacing.md),
         child: Row(
           children: [
-            Container(
-              child: Icon(
-                Icons.bolt,
-              ),
-            ),
+            Container(child: Icon(Icons.bolt)),
             Expanded(
               child: Column(
                 children: [
                   Text(
-                  "Uno", 
-                  style: AppTypography.body.copyWith(color: AppColors.primary)
+                    "Uno",
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.primary,
+                    ),
                   ),
                   Text(
                     "Clasificador v2",
-                    style: AppTypography.body.copyWith(color: AppColors.onBackground),
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.onBackground,
+                    ),
                   ),
                   Text(
                     "Hace 2 horas",
-                    style: AppTypography.body.copyWith(color: AppColors.onSurface),
+                    style: AppTypography.body.copyWith(
+                      color: AppColors.onSurface,
+                    ),
                   ),
                 ],
-              ),   
+              ),
             ),
-            Icon(
-              Icons.arrow_forward_ios
-            ),
+            Icon(Icons.arrow_forward_ios),
           ],
         ),
-        
       ),
     );
   }
 
   Widget _buildCTA() {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: AppSpacing.xs, vertical: AppSpacing.xs),
+      padding: EdgeInsets.symmetric(
+        horizontal: AppSpacing.xs,
+        vertical: AppSpacing.xs,
+      ),
       child: ElevatedButton.icon(
-        onPressed: () {}, 
+        onPressed: () {},
         icon: Icon(Icons.bolt),
         style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.primary,
@@ -188,12 +206,128 @@ class _HomeScreenState extends State<HomeScreen> {
           minimumSize: Size(double.infinity, 150),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(14),
-          )
-          
-        ),  
+          ),
+        ),
         label: Text("Nueva predicción"),
       ),
     );
   }
 
+  Widget _buildCarousel() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: AppSpacing.xs),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: AppSpacing.lg),
+            child: Row(
+              children: [Text("Modelos disponibles"), Text("Explorar modelos")],
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 160,
+                  decoration: BoxDecoration(
+                    color: AppColors.surface,
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  padding: EdgeInsets.all(AppSpacing.md),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Raking",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      Text(
+                        "Raking universidades",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.onBackground,
+                        ),
+                      ),
+                      Text(
+                        "+10 mil prediccionesç",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: AppSpacing.sm),
+                Container(
+                  width: 160,
+                  decoration: BoxDecoration(
+                    color: AppColors.surface,
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  padding: EdgeInsets.all(AppSpacing.md),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Raking",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      Text(
+                        "Raking universidades",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.onBackground,
+                        ),
+                      ),
+                      Text(
+                        "+10 mil prediccionesç",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: AppSpacing.sm),
+                Container(
+                  width: 160,
+                  decoration: BoxDecoration(
+                    color: AppColors.surface,
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  padding: EdgeInsets.all(AppSpacing.md),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Raking",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      Text(
+                        "Raking universidades",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.onBackground,
+                        ),
+                      ),
+                      Text(
+                        "+10 mil prediccionesç",
+                        style: AppTypography.body.copyWith(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: AppSpacing.sm),
+              ],
+            ),
+          ),
+          
+        ],
+      ),
+    );
+  }
 }
