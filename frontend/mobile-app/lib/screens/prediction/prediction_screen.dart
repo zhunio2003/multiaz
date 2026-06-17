4f59bf3a42fab573d783f1a28f5d0c4ba9d8bd37
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-06-16 23:53:39 -0500

    feat(frontend/mobile-app): add Prediction Screen structure

---
+++ frontend/mobile-app/lib/screens/prediction/prediction_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PredictionScreen extends StatefulWidget {
+
+  const PredictionScreen({super.key});
+
+  @override
+  State<PredictionScreen> createState() => _PredictionScreenState();
+}
+
+class _PredictionScreenState extends State<PredictionScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+  
+}
