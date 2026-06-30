530951be3d262f7af8a6b5a3bbd9bef0cfef63f5
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-06-29 23:44:05 -0500

    feat(frontend/mobile-app): add getUserId method to token service

--- frontend/mobile-app/lib/services/token_service.dart
+++ frontend/mobile-app/lib/services/token_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenService {
@@ -14,5 +16,23 @@ class TokenService {
 
   Future<String?> getRefreshToken() {
     return _storage.read(key: "refreshToken");
+  }
+
+  Future<String?> getUserId() async {
+    final token = await getAccessToken();
+
+    if (token == null) return null;
+
+    final parts = token.split('.');
+
+    if (parts.length != 3) return null;
+
+    final payload = parts[1];
+
+    final normalized = base64Url.normalize(payload);
+    final decoded = utf8.decode(base64Url.decode(normalized));
+    final map = json.decode(decoded) as Map<String, dynamic>;
+    return map['sub'] as String?;
+
   }
 }
