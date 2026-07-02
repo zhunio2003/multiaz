4496b540b3f057089ed04cd9abed649c8a8dded3
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-07-01 23:34:06 -0500

    feat(backend/ia-services/university-ranking-predictor): add structure service

---
+++ backend/ia-services/university-ranking-predictor/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from schemas.prediction_schema import PredictionRequest
+from services import inference_service
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return{"status": "UP"}
+
+@app.post("/predict")
+def predict(request: PredictionRequest):
+    return inference_service.predict(request.description)
