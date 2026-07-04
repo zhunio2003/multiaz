7d96378352d99ec0ba0726c6d8ca5f2bf55fa9e7
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-07-03 23:52:11 -0500

    fix(frontend/mobile-app): improve prediction screen - add state variables - change Text to TextEditingController - add  result and confidence

--- frontend/mobile-app/lib/screens/prediction/prediction_screen.dart
+++ frontend/mobile-app/lib/screens/prediction/prediction_screen.dart
@@ -1,34 +1,55 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_app/core/theme/app_colors.dart';
-import 'package:mobile_app/core/widgets/primary_button.dart';
-import 'package:mobile_app/core/widgets/secondary_button.dart';
+  import 'package:flutter/material.dart';
+  import 'package:mobile_app/core/theme/app_colors.dart';
+  import 'package:mobile_app/core/widgets/primary_button.dart';
+  import 'package:mobile_app/core/widgets/secondary_button.dart';
+import 'package:mobile_app/models/ai_model.dart';
+import 'package:mobile_app/models/prediction_result.dart';
 
-class PredictionScreen extends StatefulWidget {
+  class PredictionScreen extends StatefulWidget {
 
-  const PredictionScreen({super.key});
+    final AiModel aiModel;
 
-  @override
-  State<PredictionScreen> createState() => _PredictionScreenState();
-}
 
-class _PredictionScreenState extends State<PredictionScreen> {
+    const PredictionScreen({
+      super.key, 
+      required this.aiModel,
+    });
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.background,
-      body: SafeArea(
-        child: Row(
-          children: [
-            Text("Prediction"),
-            Text("RESULT"),
-            Text("Score"),
-            PrimaryButton(label: "Ver mas"),
-            SecondaryButton(label: "Volver a predicir")
-          ],
-        )
-        ),
-    );
+    @override
+    State<PredictionScreen> createState() => _PredictionScreenState();
   }
-  
-}
+
+  class _PredictionScreenState extends State<PredictionScreen> {
+
+    bool _isLoading = false;
+    PredictionResult? _result = null;
+
+    final TextEditingController _titleController = TextEditingController();
+    final TextEditingController _textController = TextEditingController();
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: AppColors.background,
+        body: SafeArea(
+          child: Column(
+            children: [
+              TextField(controller: _titleController, decoration: InputDecoration(labelText: "title")),
+              TextField(controller: _textController, decoration: InputDecoration(labelText: "text")),
+              PrimaryButton(label: "Predecir", onPressed: () {}),
+              SecondaryButton(label: "Volver a predicir", onPressed: () {}),
+              _result != null ? Text("Resultado: ${_result?.result}, Score: ${_result?.confidence}") : const SizedBox.shrink(),
+            ],
+          )
+          ),
+      );
+    }
+
+    @override
+    dispose() {
+      _titleController.dispose();
+      _textController.dispose();
+      super.dispose();
+    }
+    
+  }
