518b7e86b20bcd53f2d48dc76ff8d2f0c861ad30
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-07-06 22:46:45 -0500

    feat(frontend/mobile-app): add /predict router to app_router

--- frontend/mobile-app/lib/core/navigation/app_router.dart
+++ frontend/mobile-app/lib/core/navigation/app_router.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/models/ai_model.dart';
 import 'package:mobile_app/screens/auth/forgot_password_screen.dart';
 import 'package:mobile_app/screens/auth/login_screen.dart';
 import 'package:mobile_app/screens/auth/register_screen.dart';
 import 'package:mobile_app/screens/auth/reset_password_screen.dart';
 import 'package:mobile_app/screens/catalog/model_catalog_screen.dart';
 import 'package:mobile_app/screens/home/home_screen.dart';
+import 'package:mobile_app/screens/prediction/prediction_screen.dart';
 
 class AppRouter {
 
@@ -19,6 +21,10 @@ class AppRouter {
     '/forgot-password': (context) => ForgotPasswordScreen(),
     '/reset-password': (context) => ResetPasswordScreen(),
     '/catalog': (context) => ModelCatalogScreen(),
+    '/predict': (context) {
+      final model = ModalRoute.of(context)!.settings.arguments as AiModel;
+      return PredictionScreen(aiModel: model);
+    }
   };
 
 }
