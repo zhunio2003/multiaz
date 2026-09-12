fa638b71ac484c585a4835cd148835d4f3456c9a
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-09-11 23:02:06 -0500

    feat(backend/core/prediction-storage): add structure service

---
+++ backend/core/prediction-storage/src/main/java/com/multiaz/predictionstorage/PredictionStorageApplication.java
@@ -0,0 +1,13 @@
+package com.multiaz.predictionstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionStorageApplication.class, args);
+	}
+
+}
---
+++ backend/core/prediction-storage/src/test/java/com/multiaz/predictionstorage/PredictionStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiaz.predictionstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
