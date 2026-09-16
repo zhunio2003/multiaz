2c58bdca4e820e17ed42f1205bafb93d9ac12ae9
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-09-15 23:30:00 -0500

    feat(backend/core/dataset-management): add structure service

---
+++ backend/core/dataset-management/src/main/java/com/multiaz/datasetmanagement/DatasetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.multiaz.datasetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasetManagementApplication.class, args);
+	}
+
+}
