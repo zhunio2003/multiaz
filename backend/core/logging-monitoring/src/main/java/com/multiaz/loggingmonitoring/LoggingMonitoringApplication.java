7aa06f00866b8b4f5f2cf6a600c98ec1538cbd28
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2026-09-17 23:30:00 -0500

    feat(backend/core/logging-monitoring): add initial structure

---
+++ backend/core/logging-monitoring/src/main/java/com/multiaz/loggingmonitoring/LoggingMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.multiaz.loggingmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingMonitoringApplication.class, args);
+	}
+
+}
---
+++ backend/core/logging-monitoring/src/test/java/com/multiaz/loggingmonitoring/LoggingMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiaz.loggingmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
